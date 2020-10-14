19bddd2cd09e18fd1581042d3a7ac22db922744f
Rishit Khandelwal <65018379+rishit-khandelwal@users.noreply.github.com>
2020-10-14 11:46:22 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const { Octokit } = require("@octokit/core");
+const Discord = require("discord.js");
+var hfbot = new Discord.Client()
+var unirest = require('unirest');
+
+hfbot.once('ready', () => {
+	console.log('Ready!');
+});
+const octokit = new Octokit({ auth: `` });
+hfbot.on('message', async (msg) => {
+    var content = msg.content;
+    if (content.startsWith(':')) {
+        content = content.slice(1,content.length).split(' ')
+        switch(content[0]) {
+            case "q":
+                const repo = content[1];
+                if (!repo) {
+                    return
+                }
+                var resp = await octokit.request('GET /repos/{owner}/{repo}/topics', {
+                        owner: content[1].split('/')[0],
+                        repo: content[1].split('/')[1],
+                        mediaType: {
+                        previews: [
+                            'mercy'
+                        ]
+                    }
+                });
+                
+                const topics = resp["data"]["names"];
+                for (var topic of topics)
+                    if (topic == "hacktoberfest" || topic == "hacktoberfest2020") {
+                        msg.channel.send(`Yep`)
+                        return;
+                    }
+                    else {
+                        continue;                
+                    }
+            break;
+        }
+    }
+})
+
+hfbot.login("NzY1NjEyMjU5MzA4MjA4MTgw.X4XV-w.SWCfbOioRbhxfvQPj7pZbvHR-Go");
