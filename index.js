194504a348c0f8e323d05c74056c2ddfe8050203
Rishit Khandelwal <65018379+rishit-khandelwal@users.noreply.github.com>
2020-10-14 17:46:23 +0530

    Added features

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var hfbot = new Discord.Client()
 var unirest = require('unirest');
 
 hfbot.once('ready', () => {
+    hfbot.user.setActivity("Watching out for :q help")
 	console.log('Ready!');
 });
 const octokit = new Octokit({ auth: `` });
@@ -14,12 +15,36 @@ hfbot.on('message', async (msg) => {
         switch(content[0]) {
             case "q":
                 const repo = content[1];
+                if (repo == "help")
+                {
+                    msg.channel.send(
+                        "Hello, I am Hacktoberbot\n\
+Send a message with\n\
+```\
+:q repo_owner/repo_name\
+```\
+**or**\n\
+```\
+:q <SLAP_THE_GITHUB_LINK_HERE>\
+```"
+                    )
+                    return;
+                }
                 if (!repo) {
                     return
                 }
+                var owner;
+                var repos;
+                if (content[1].startsWith("https")) {
+                    owner = content[1].slice("https://github.com/".length, content[1].length).split("/")[0]
+                    repos = content[1].slice("https://github.com/".length, content[1].length).split("/")[1]
+                } else {
+                    owner = content[1].split('/')[0]
+                    repos = content[1].split('/')[1]
+                }
                 var resp = await octokit.request('GET /repos/{owner}/{repo}/topics', {
-                        owner: content[1].split('/')[0],
-                        repo: content[1].split('/')[1],
+                        owner: owner,
+                        repo: repos,
                         mediaType: {
                         previews: [
                             'mercy'
@@ -30,11 +55,11 @@ hfbot.on('message', async (msg) => {
                 const topics = resp["data"]["names"];
                 for (var topic of topics)
                     if (topic == "hacktoberfest" || topic == "hacktoberfest2020") {
-                        msg.channel.send(`Yep`)
+                        msg.channel.send(`Yep\n`)
                         return;
                     }
                     else {
-                        continue;                
+                        continue;  
                     }
             break;
         }
