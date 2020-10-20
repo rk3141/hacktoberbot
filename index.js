91badb5089017a5a444a6c75f2b00f422a711c11
Rishit Khandelwal <65018379+rishit-khandelwal@users.noreply.github.com>
2020-10-20 08:31:21 +0530

    Added some comments

--- index.js
+++ index.js
@@ -12,17 +12,11 @@ hfbot.once('ready', () => {
 });
 
 const octokit = new Octokit({ auth: `` });
-hfbot.on('message', async (msg) => {
-    var content = msg.content;
-    if (content.startsWith(':')) {
-        content = content.slice(1,content.length).split(' ')
-        switch(content[0]) {
-            case "q":
-                const repo = content[1];
-                if (repo == "help")
-                {
-                    msg.channel.send(
-                        "Hello, I am Hacktoberbot\n\
+
+function help(message)
+{
+    message.channel.send(
+        "Hello, I am Hacktoberbot\n\
 Send a message with\n\
 ```\
 :q repo_owner/repo_name\
@@ -30,25 +24,41 @@ Send a message with\n\
 **or**\n\
 ```\
 :q <SLAP_THE_GITHUB_LINK_HERE>\
-```"
-                    )
+    ```\
+\nIf you face issues it might be due to GitHub API rate limits.")
+}
+
+hfbot.on('message', async (msg) => {
+    var content = msg.content;
+    if (content.startsWith(':')) {
+        
+        content = content.slice(1,content.length).split(' ') // Remove the colon and split the rest by ' '
+
+        switch(content[0]) {
+            case "q":
+                const repo = content[1];
+                if (repo == "help")
+                {
+                    help(msg);
+                    
                     return;
                 }
-                if (!repo) {
+
+                if (!repo) { // Just so that no one is messing around
                     return
                 }
+
                 var owner;
                 var repos;
                 if (content[1].startsWith("https")) {
                     owner = content[1].slice("https://github.com/".length, content[1].length).split("/")[0]
                     repos = content[1].slice("https://github.com/".length, content[1].length).split("/")[1]
                 } else {
-                    console.log('DBG',content)
                     owner = content[1].split('/')[0]
                     repos = content[1].split('/')[1]
                 }
-                var resp = await octokit.request('GET /repos/{owner}/{repo}/topics', {
-                        owner: owner,
+                var resp = await octokit.request('GET /repos/{owner}/{repo}/topics', { // make an request to the github api
+                        owner: owner,                                                  // You will have to authenticate to have have higher rate
                         repo: repos,
                         mediaType: {
                         previews: [
@@ -57,8 +67,8 @@ Send a message with\n\
                     }
                 });
                 
-                const topics = resp["data"]["names"];
-                for (var topic of topics) {
+                const topics = resp["data"]["names"]; // this is the array of topics
+                for (var topic of topics) { // topics.includes didn't work
                     console.log(topic)
                     if (topic == "hacktoberfest" || topic == "hacktoberfest2020") {
                         msg.channel.send(`Yep\n`)
@@ -68,6 +78,7 @@ Send a message with\n\
                         continue;  
                     }
                 }
+                msg.channel.send('Nope\n');
             break;
         }
     }
